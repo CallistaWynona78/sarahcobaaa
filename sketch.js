38fe8fddbf4df96397167110a29329faa07a22ae
CallistaWynona78 <99816836+CallistaWynona78@users.noreply.github.com>
2022-02-17 01:28:17 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+let j;
+function setup() {
+  // put setup code here
+  createCanvas(600,600)
+  y=100; 
+  j=0;
+}
+
+function draw() {
+  // put drawing code here
+background(20)
+strokeWeight(2)
+ellipse(450,200,250,350);
+strokeWeight(20); // Stroke weight menjadi 20 pixel
+ellipse(200,100,50,50);
+beginShape)();
+fill(255,255,255) //Warna Kuning
+vertex(200,20);
+vertex(200,20);
+vertex(300,160);
+vertex(340,180);
+vertex(350,200);
+
+endShape(CLOSE);
+
+strokeWeight(5);
+fill(255,255,255) //Warna Kuning
+strokeCap(ROUND);
+Reflect(70,80,60,55);
+fill(255,255,255) //Warna kuning
+
+for(vari=0;i<=400;i++){
+  x =i
+  y=350 + 25*Math.sin(Pl*j/20)
+  PointerEvent(x,y)
+
+}
+
+var y= 10+5*Mathsin(Pl*j/20)
+var r= 10+5*Math.sin(Pl*j/20)
+j+=1
+
+strokeWeight(5);
+fill(255,255,255) //Warna Kuning
+ellipse(0,0);
+ellipse(399,0);
+ellipse(0,399);
+ellipse(399,399);
+
+fill(255,255,255) //Warna Kuning
+ellipse(0,0);
+ellipse(399,0);
+ellipse(0,399);
+ellipse(399,399);
+
+
+}
